c68da85a0f5da80ea7e821d8e7a8d43e219e93bb
Your Name <you@example.com>
2019-09-08 12:50:17 +0000

    get tick data

--- stock/tick-data/get_stock_tick.py
+++ stock/tick-data/get_stock_tick.py
@@ -1,5 +1,6 @@
 import tushare as ts
 import re
+import datetime
 def get_tick_data_by_date(code, date):
     '''
     parameters
@@ -28,12 +29,13 @@ def get_tick_data_by_range(code, date_b, date_e):
             DataFrame 属性: 成交时间、成交价格、价格变动、成交手、成交金额、买卖类型
     '''
     begin=re.findall(r"\d+", date_b)
-    begin=datetime.date(begin[0],begin[1],begin[2])
+    begin=datetime.date(int(begin[0]),int(begin[1]),int(begin[2]))
     end=re.findall(r"\d+", date_e)
-    end=datetime.date(end[0],end[1],end[2])
+    end=datetime.date(int(end[0]),int(end[1]),int(end[2]))
     results={}
     for i in range((end-begin).days+1):
         date = begin + datetime.timedelta(days=i)
+        date=date.strftime("%Y-%m-%d")
         results[date]=ts.get_tick_data(code, date, 3, 1, 'tt')
     return results
 
@@ -55,6 +57,12 @@ def get_tick_data(code, date_b, date_e=None):
         同get_tick_data_by_date
     '''
     if date_e==None:
-        return ts.get_tick_data(code, date, 3, 1 , 'tt')
+        return ts.get_tick_data(code, date_b, 3, 1 , 'tt')
     else:
         return get_tick_data_by_range(code, date_b, date_e)
+
+if __name__== '__main__':
+    print(get_tick_data('600848', '2019-09-01'))
+    print(get_tick_data('600848', '2019-09-01', '2019-09-10'))
+    print(get_tick_data_by_date('600848', '2019-09-01'))
+    print(get_tick_data_by_range('600848', '2019-09-01', '2019-09-10'))
