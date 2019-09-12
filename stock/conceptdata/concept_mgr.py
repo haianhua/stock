5b3457ae03ca47f3316cfeb83cd0766589aea8e7
Your Name <you@example.com>
2019-09-12 15:36:15 +0000

    init

--- stock/conceptdata/concept_mgr.py
+++ stock/conceptdata/concept_mgr.py
@@ -5,66 +5,80 @@ import tushare as ts
 import re
 import datetime
 import basicdata.basic_mgr as sk
+import time
+import os
+import pandas as pd
 
-ctcode_name=None
-#ctcode_name['TS56']='电改'
-tscode_concept=None
-#tscode_concept['000008.SZ']={'id':['TS56','TS59'],'name':['电改','特斯拉']}
+g_update_newest=False #True|False
+#是否下载最新的概念，一般不需要
+
+g_ctcode_name=None
+#g_ctcode_name['TS56']='电改'
+
+g_tscode_concept=None
+#g_tscode_concept['000008.SZ']={'id':['TS56','TS59'],'name':['电改','特斯拉']}
 
 def get_ctname_by_tscode(pro, tscode):
-    global tscode_concept
-    if tscode_concept is None:
+    global g_tscode_concept
+    if g_tscode_concept is None:
         init_tscode_concept(pro)
-    return tscode_concept[tscode]['name']
+    return g_tscode_concept[tscode]['name']
 
 def get_ctcode_by_tscode(pro, tscode):
-    global tscode_concept
-    if tscode_concept is None:
+    global g_tscode_concept
+    if g_tscode_concept is None:
         init_tscode_concept(pro)
-    return tscode_concept[tscode]['id']
+    return g_tscode_concept[tscode]['id']
 
 def init_tscode_concept(pro):
-    global tscode_concept
-    if tscode_concept is None:
-        tscode_concept = {}
+    global g_tscode_concept
+    global g_update_newest
+    if g_tscode_concept is None:
+        g_tscode_concept = {}
         ts_codes=sk.get_tscodes(pro)
         for i in range(len(ts_codes)):
             ts_code=ts_codes[i]
-            conceptdf=pro.concept_detail(ts_code=ts_code)
+            path='./concept-data/'+ts_code+'.concept.csv'
+            if g_update_newest == False and os.path.exists(path) == True:
+                conceptdf=pd.read_csv(path)
+            else:
+                conceptdf=pro.concept_detail(ts_code=ts_code)
+                if conceptdf is not None:
+                    conceptdf.to_csv(path)
+                time.sleep(1)
+                print("download", path)
             if conceptdf is not None:
-                conceptdf.to_csv('./concept-data/'+ts_code+'.concept.csv')
                 conceptids=conceptdf['id'].values.tolist()
                 conceptnames=conceptdf['concept_name'].values.tolist()
-                tscode_concept[ts_code]={'id':conceptids, 'name':conceptnames}
-            time.sleep(1)
+                g_tscode_concept[ts_code]={'id':conceptids, 'name':conceptnames}
 
 
 def get_concept_map(pro):
-    global ctcode_name
-    if ctcode_name is None:
+    global g_ctcode_name
+    if g_ctcode_name is None:
         init_ctcode_name(pro)
-    return ctcode_name
+    return g_ctcode_name
 
 def get_name(pro, code):
-    global ctcode_name
-    if ctcode_name is None:
+    global g_ctcode_name
+    if g_ctcode_name is None:
         init_ctcode_name(pro)
-    return ctcode_name[code]
+    return g_ctcode_name[code]
 
 def init_ctcode_name(pro):
-    global ctcode_name
-    if ctcode_name is None:
-        ctcode_name = {}
+    global g_ctcode_name
+    if g_ctcode_name is None:
+        g_ctcode_name = {}
         conceptdf=pro.concept(src='ts')
         conceptcodes=conceptdf['code'].values.tolist()
         conceptnames=conceptdf['name'].values.tolist()
         for i  in range(len(conceptcodes)):
-            ctcode_name[conceptcodes[i]]= conceptnames[i]
+            g_ctcode_name[conceptcodes[i]]= conceptnames[i]
 
 if __name__== '__main__':
     pro = ts.pro_api('08aedc1cc54171e54a64bbe834ec1cb45026fa2ab39e9e4cb8208cad')
     init_ctcode_name(pro)
-    print(ctcode_name)
+    print(g_ctcode_name)
     print(get_name(pro, 'TS2'))
     print(get_ctcode_by_tscode(pro, '600848.SH'))
     #conceptdf.to_csv('./concept.csv')
